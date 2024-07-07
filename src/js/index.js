d275e3013530de88078fa480b68cfdefc7335619
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2024-07-07 11:29:21 +0300

    Mobile complete

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,30 @@
 // });
 
 
-import menuActive from "./modules/menu-active.js";
-menuActive();
+// import menuActive from "./modules/menu-active.js";
+// menuActive();
 
-import customers from "./modules/customers.js";
-customers();
+// import customers from "./modules/customers.js";
+// customers();
+
+
+// Мобильная навигация
+const navBtn = document.querySelector(".mobile-nav-btn");
+const nav = document.querySelector(".mobile-nav");
+const menuIcon = document.querySelector(".nav-icon");
+
+const fade = document.querySelector(".mobile-nav-fade");
+
+navBtn.onclick = toggleMobile;
+fade.onclick = toggleMobile;
+
+function toggleMobile() {
+    nav.classList.toggle("mobile-nav--open");
+    menuIcon.classList.toggle("nav-icon--active");
+    document.body.classList.toggle("no-scroll");
+    fade.classList.toggle("mobile-nav-fade--open");
+}
+
 
 // import btnStatus from "./modules/btn-status.js";
 // btnStatus();
